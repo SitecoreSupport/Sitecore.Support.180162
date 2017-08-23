10df07f9d1b6a9e05378aa0584383cf48e2911a8
DK\yho <yho@sitecore.net>
2017-08-23 17:24:38 +0800

    Commit patch

---
+++ src/Sitecore.Support.180162/CustomCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Events.Hooks;
+
+namespace Sitecore.Support
+{
+    public class CustomCreator
+    {
+        public void Initialize()
+        {
+            Sitecore.Resources.Media.MediaManager.Creator = new CustomMediaCreator();
+        }
+    }
+}
---
+++ src/Sitecore.Support.180162/CustomMediaCreator.cs
@@ -0,0 +1,66 @@
+﻿using Sitecore;
+using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.IO;
+using Sitecore.Pipelines.GetMediaCreatorOptions;
+using Sitecore.Resources.Media;
+using Sitecore.SecurityModel;
+using System;
+using System.IO;
+
+namespace Sitecore.Support
+{
+    public class CustomMediaCreator : MediaCreator
+    {
+        protected override Item CreateFolder(string itemPath, MediaCreatorOptions options)
+        {
+            Assert.ArgumentNotNullOrEmpty(itemPath, "itemPath");
+            Assert.ArgumentNotNull(options, "options");
+            Item item2;
+            using (new SecurityDisabler())
+            {
+                TemplateItem folderTemplate = this.GetFolderTemplate(options);
+                Database database = this.GetDatabase(options);
+                Item item = database.GetItem(itemPath, options.Language);
+                if (item != null)
+                {
+                    return item;
+                }
+                item2 = database.CreateItemPath(itemPath, folderTemplate, folderTemplate);
+                Assert.IsNotNull(item2, typeof(Item), "Could not create media folder: '{0}'.", new object[]
+                {
+                    itemPath
+                });
+            }
+            return item2;
+        }
+
+        private TemplateItem GetFolderTemplate(MediaCreatorOptions options)
+        {
+            Assert.ArgumentNotNull(options, "options");
+            Database database = this.GetDatabase(options);
+            var language = options.Language ?? Context.Language;
+            TemplateItem templateItem = database.Templates[TemplateIDs.MediaFolder, language];
+            Assert.IsNotNull(templateItem, typeof(TemplateItem), "Could not find folder template for media. Template: '{0}'", new object[]
+            {
+                TemplateIDs.MediaFolder
+            });
+            return templateItem;
+        }
+
+        private Database GetDatabase(MediaCreatorOptions options)
+        {
+            Assert.ArgumentNotNull(options, "options");
+            Database resultDatabase;
+            if ((resultDatabase = options.Database) == null)
+            {
+                resultDatabase = (Context.ContentDatabase ?? Context.Database);
+            }
+            return Assert.ResultNotNull<Database>(resultDatabase);
+        }
+    }
+}
